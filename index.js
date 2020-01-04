25c69417f7fac9d061562d1a1a45d1d1121ccd0b
mr andersen <chasea523@gmail.com>
2020-01-04 02:36:48 -0800

    added questions and npm packages

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
+//npm packages
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
+
+
+
+
+
+//Questions for prompt
+
 const questions = [
-  
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username"
+    },
+    {
+        type: "list",
+        name: "colors",
+        message:"What is your favorite color?",
+        choices: ["green", "blue", "pink", "red"]
+    }
 ];
 
 function writeToFile(fileName, data) {
