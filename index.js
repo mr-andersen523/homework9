d17d0919613255e83e691190b521ca1be2f32f2c
mr andersen <chasea523@gmail.com>
2020-01-06 22:57:30 -0800

    added github query

--- index.js
+++ index.js
@@ -36,6 +36,73 @@ function init() {
     .then(function ({username, color}) {
         const queryUrl = `https://api.github.com/users/${username}`; 
 
+        axios
+            .get(queryUrl)
+            .then((res) => {    
+                // console.log(res.data)
+
+                
+
+                switch(color) {
+                    case 'green':
+                        data.color = 0;
+                        break;
+                    case 'blue':
+                        data.color = 1;
+                        break;  
+                    case 'pink':
+                        data.color = 2;
+                        break;
+                    case 'red':
+                        data.color = 3;
+                        break;
+                }      
+                // console.log(data.color)  
+
+                data.username = username;
+                data.numOfRepo = res.data.public_repos;
+                data.name = res.data.name
+                data.followers = res.data.followers;
+                data.following = res.data.following;
+                data.portPic = res.data.avatar_url;
+                data.location = res.data.location;
+                data.blog = res.data.blog; 
+                data.company = res.data.company
+                data.bio = res.data.bio
+
+                axios //  axios call for stars
+                    .get(`https://api.github.com/users/${username}/repos?per_page=100`)
+                    .then((res) => {
+                        // console.log(res)
+                        data.stars = 0;
+                        for (let i = 0; i < res.data.length; i++) { // Loop through each repository and count the number of stars
+                            data.stars += res.data[i].stargazers_count;
+                        }
+                        
+
+                        // console.log(data.stars)
+
+                        let resumeHTML = generateHTML(data);
+                        // console.log(resumeHTML)
+
+                        conversion({ html: resumeHTML }, function(err, result) {
+                            if (err) {
+                              return console.error(err);
+                            }
+                           
+                            console.log(result.numberOfPages);
+                            console.log(result.logs);
+                            result.stream.pipe(fs.createWriteStream('./resume.pdf'));
+                            conversion.kill(); // necessary if you use the electron-server strategy, see below for details
+                          });
+                    })
+
+            })
+    })
+}
+
+
+init();
 
 
 
@@ -58,19 +125,3 @@ function init() {
 //       conversion.kill(); // necessary if you use the electron-server strategy, see bellow for details
 //     });
 // };
-
-
-
-
-
-
-
-
-
-function writeToFile(fileName, data) {
- 
-}
-
-function init() {
-
-init();
